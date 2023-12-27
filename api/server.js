260afb9ca4f1c9bd31fadd9a057dfca87b0c2c3e
Trigo <trigoyodila1996@gmail.com>
2023-12-27 11:47:25 +0100

    main : add employes routes

--- api/server.js
+++ api/server.js
@@ -1,7 +1,11 @@
 const express = require("express")
+const employesRouter = require("./routes/employes")
 const dotenv = require('dotenv').config()
-const port = 5000
+const port = process.env.PORT || 5000
+
 
 const app = express()
 
-app.listen(port, () => console.log(`Server started on port ${port}`))
+app.use('/api/employes', employesRouter)
+
+app.listen(port, () => console.log(`Server started on port ${port}`)) 
