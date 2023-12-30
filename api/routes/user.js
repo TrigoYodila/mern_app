f7cbd159ad7c20d0b643b845306d0f3a856b70b6
Trigo <trigoyodila1996@gmail.com>
2023-12-30 10:32:25 +0100

    fix auth middleware

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const { registerUser, loginUser, getMe } = require('../controllers/user')
+const { protect } = require('../middleware/auth')
 
 const router = express.Router()
 
 router.post('/', registerUser)
 router.post('/login', loginUser)
-router.get('/me', getMe)
+router.get('/me', protect,getMe)
 
 module.exports = router
